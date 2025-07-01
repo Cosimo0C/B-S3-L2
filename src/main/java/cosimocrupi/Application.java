9c855ac371e3f96d03026611e9fe98bff314b88a
Cosimo0C <cosimocrupi01@gmail.com>
2025-07-01 16:53:37 +0200

    almost done methods and table

--- src/main/java/cosimocrupi/Application.java
+++ src/main/java/cosimocrupi/Application.java
@@ -5,7 +5,7 @@ import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Persistence;
 
 public class Application {
-private static final EntityManagerFactory emf= Persistence.createEntityManagerFactory("B-S2-L2");
+private static final EntityManagerFactory emf= Persistence.createEntityManagerFactory("B-S3-L2");
 private static final EntityManager em= emf.createEntityManager();
     public static void main(String[] args) {
 
--- src/main/java/cosimocrupi/entities/Evento.java
+++ src/main/java/cosimocrupi/entities/Evento.java
@@ -9,11 +9,29 @@ import java.time.LocalDate;
 public class Evento{
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ID")
     private long id;
+
+    @Column(name = "Titolo")
     String titolo;
+
+    @Column(name = "Data evento")
     LocalDate dataEvento;
+
+    @Column(name = "Descrizione")
     String descrizione;
+
+    @Column(name = "Tipo di evento")
     @Enumerated(EnumType.STRING)
     Tipo tipoEvento;
+
+    @Column(name = "Max partecipanti")
     int numeroMassimoPartecipanti;
+    public Evento(String titolo, LocalDate dataEvento, String descrizione, Tipo tipoEvento, int numeroMassimoPartecipanti){
+        this.titolo=titolo;
+        this.dataEvento=dataEvento;
+        this.descrizione=descrizione;
+        this.tipoEvento=tipoEvento;
+        this.numeroMassimoPartecipanti=numeroMassimoPartecipanti;
+    }
 }
