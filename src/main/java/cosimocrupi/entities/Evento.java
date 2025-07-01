fcb77240b386679cc461a6145083bcadcb31c4d3
Cosimo0C <cosimocrupi01@gmail.com>
2025-07-01 17:05:10 +0200

    done methods and  done table

--- src/main/java/cosimocrupi/entities/Evento.java
+++ src/main/java/cosimocrupi/entities/Evento.java
@@ -35,6 +35,18 @@ public class Evento{
         this.numeroMassimoPartecipanti=numeroMassimoPartecipanti;
     }
 
+    @Override
+    public String toString() {
+        return "Evento{" +
+                "id=" + id +
+                ", titolo='" + titolo + '\'' +
+                ", dataEvento=" + dataEvento +
+                ", descrizione='" + descrizione + '\'' +
+                ", tipoEvento=" + tipoEvento +
+                ", numeroMassimoPartecipanti=" + numeroMassimoPartecipanti +
+                '}';
+    }
+
     public long getId() {
         return id;
     }
@@ -42,9 +54,15 @@ public class Evento{
     public String getTitolo() {
         return titolo;
     }
+    public void setTitolo(String titolo) {
+        this.titolo = titolo;
+    }
 
     public LocalDate getDataEvento() {
         return dataEvento;
+    }
+    public void setDataEvento(LocalDate dataEvento) {
+        this.dataEvento = dataEvento;
     }
 
     public String getDescrizione() {
@@ -54,12 +72,21 @@ public class Evento{
             return descrizione;
         }
     }
+    public void setDescrizione(String descrizione) {
+        this.descrizione = descrizione;
+    }
 
     public Tipo getTipoEvento() {
         return tipoEvento;
     }
+    public void setTipoEvento(Tipo tipoEvento) {
+        this.tipoEvento = tipoEvento;
+    }
 
     public int getNumeroMassimoPartecipanti() {
         return numeroMassimoPartecipanti;
+    }
+    public void setNumeroMassimoPartecipanti(int numeroMassimoPartecipanti) {
+        this.numeroMassimoPartecipanti = numeroMassimoPartecipanti;
     }
 }
